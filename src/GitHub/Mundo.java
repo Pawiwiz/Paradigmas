1568f03b49a1bc416e9409b70492b72b053d52bf
Paulina Herrera Colín <pauherrera10okey@gmail.com>
2022-08-24 09:49:15 -0500

    Java

--- src/GitHub/Mundo.java
+++ src/GitHub/Mundo.java
@@ -2,6 +2,6 @@ package GitHub;
 
 public class Mundo {
     public static void main(String[] args) {
-        System.out.println("Hola mundo java");
+        System.out.println("Hola mundo java jijiji");
     }
 }
